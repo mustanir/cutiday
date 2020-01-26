b88bb456ab6aef3264539a7969cc94b42ac5ffcd
Arsyad bin Anuar <syaads@yahoo.com.my>
2020-01-27 00:02:15 +0800

    - Remove Button component in Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,6 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import {  makeStyles } from '@material-ui/core/styles';
 import { LoginDialog } from './LoginDialog';
